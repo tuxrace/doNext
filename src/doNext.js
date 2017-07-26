35e91efc88c6dc29bd54fe925f4e4c585797cf23
Gerard Banasig <gerardb@istackmanila.com>
2017-07-26 18:55:01 +0800

    reformat code style

--- src/doNext.js
+++ src/doNext.js
@@ -2,18 +2,18 @@ function callNext (gen, prev) {
   if (prev.done === false) {
     prev.value
       .then((result) => {
-        callNext(gen, gen.next(result))
-      })
+        callNext(gen, gen.next(result));
+      });
   }
 }
 
 function doNext (genFunc) {
-  const gen = genFunc()
-  const startGen = gen.next()
+  const gen = genFunc();
+  const startGen = gen.next();
   startGen.value
     .then((r) => {
-      callNext(gen, gen.next(r))
-    })
+      callNext(gen, gen.next(r));
+    });
 }
 
-module.exports = doNext
+module.exports = doNext;
--- test.js
+++ test.js
@@ -1,6 +1,7 @@
+const { describe, it } = require('mocha');
 const assert = require('assert');
-describe('run app without errors',() => {
-    it('should run without errors',() => {
-        assert.equal(1,1)
-    })
-})
+describe('run app without errors', () => {
+  it('should run without errors', () => {
+    assert.equal(1, 1);
+  });
+});
