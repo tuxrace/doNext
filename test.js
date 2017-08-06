7128485b125b4d26e8983d1e8c08a3d1ff5cfe44
Gerard Banasig <tuxrace@yahoo.com>
2017-08-06 20:40:34 +0800

    add/test

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 const { describe, it } = require('mocha');
+const doNext = require('./src/doNext');
+const fetch = require('isomorphic-fetch');
+
 const assert = require('assert');
 describe('run app without errors', () => {
   it('should run without errors', () => {
-    assert.equal(1, 1);
+    assert.equal(1,2)
+    
   });
 });
